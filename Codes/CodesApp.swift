4c738cd8598117a5d949b0083114959f6e63988f
Musaab mohammed <musaab.aljarba9@gmail.com>
2021-04-28 20:39:32 +0300

    Initial Commit

---
+++ Codes/CodesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodesApp.swift
+//  Codes
+//
+//  Created by Musab Aljarba on 16/09/1442 AH.
+//
+
+import SwiftUI
+
+@main
+struct CodesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Codes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Codes
+//
+//  Created by Musab Aljarba on 16/09/1442 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
