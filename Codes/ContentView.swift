27244b4b33fd44c9200b83465ad5b3d55683ed1e
Musaab mohammed <musaab.aljarba9@gmail.com>
2021-05-10 22:11:33 +0300

    Applying list on tap highlight edisappear on main menue before playing with the main stores list

--- Codes/ContentView.swift
+++ Codes/ContentView.swift
@@ -14,7 +14,7 @@ extension List {
         if #available(iOS 14.0, *) { // iOS 14
             self
             .accentColor(Color.secondary)
-            .listStyle(SidebarListStyle())
+            .listStyle(PlainListStyle())
             .onAppear {
                 UITableView.appearance().backgroundColor = UIColor.systemBackground
             }
@@ -41,19 +41,35 @@ struct ContentView: View {
     
     @StateObject private var regesterVM = CreateUserViewModel()
     
+    @State var searchText = ""
+    @State var isSearching = false
+    
+    @State var selection: String? = nil
+    
+    
     var body: some View {
         
-            List(storeListVM.stores, id: \.storeId) { store in
+        VStack {
+            SearchBar(searchText: $searchText, isSearching: $isSearching)
+
+            List(storeListVM.stores.filter({
+                "\($0)".contains(searchText) || searchText.isEmpty
+            }), id: \.storeId) { store in
                 NavigationLink(
-                    destination: StoreCodesListView(store: store),
-                    label: {
+                    destination: StoreCodesListView(store: store), tag: store.storeId, selection: $selection) {
                     StoreCell(store: store)
-                    })
+                }
+                .onDisappear {
+                    self.selection = nil
+                }
+                
             }
             .noSeparators()
             .navigationBarTitleDisplayMode(.large)
             .navigationBarTitle("المتاجر")
-            .embedInNavigationView()
+        }
+        
+        .embedInNavigationView()
         
         
         .onAppear {
@@ -75,12 +91,12 @@ struct StoreCell: View {
             Image(store.picture)
                 .resizable()
                 .scaledToFit()
-                .frame(width: 75, height:75)
+                .frame(width: 65, height: 65)
                 .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
                 .padding(5)
             VStack(alignment: .leading, spacing: nil) {
                 Text(store.name)
-                    .font(.title3).bold()
+                    .font(.body).bold()
                     .padding(1)
                 Text("عدد الاكواد")
                     .font(.footnote)
@@ -92,6 +108,7 @@ struct StoreCell: View {
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView().environment(\.layoutDirection, .rightToLeft)
@@ -99,3 +116,62 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
+
+struct SearchBar: View {
+    @Binding var searchText: String
+    @Binding var isSearching: Bool
+    
+    var body: some View {
+        HStack {
+            HStack {
+                TextField("بحث...", text: $searchText)
+                    .padding(isSearching ? 10 : 7)
+                    .padding(.horizontal, 25)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .padding(.horizontal, 10)
+            }
+            
+            .onTapGesture {
+                isSearching = true
+            }
+            .overlay(
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                        .rotationEffect(.degrees(-90))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 16)
+                    
+                    if isSearching {
+                        Button(action: {
+                            searchText = ""
+                        }, label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .padding(.trailing, 16)
+                                .padding(.vertical)
+                        })
+                        
+                        
+                    }
+                }
+                .foregroundColor(.gray)
+            ).transition(.move(edge: .trailing))
+            .animation(.default)
+            
+            if isSearching {
+                Button(action: {
+                    isSearching = false
+                    searchText = ""
+                    
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }, label: {
+                    Text("إلغاء")
+                        .padding(.trailing)
+                        .padding(.leading, -5)
+                })
+                .transition(.move(edge: .trailing))
+                .animation(.default)
+            }
+        }
+    }
+}
---
+++ Codes/Views/MainView.swift
@@ -0,0 +1,22 @@
+//
+//  MainView.swift
+//  Codes
+//
+//  Created by Musab Aljarba on 27/09/1442 AH.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
